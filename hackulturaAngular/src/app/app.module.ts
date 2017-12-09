47c8aefb787599f857660afd8605f998ed9319f5
Michael Barroso <michael_p._b.15600@hotmail.com>
2017-12-09 03:29:06 -0200

    Adicionando materialize

--- hackulturaAngular/src/app/app.module.ts
+++ hackulturaAngular/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AuthGuard } from './guard/auth.guard';
+import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,8 @@ import { AuthGuard } from './guard/auth.guard';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule
   ],
   providers: [AuthGuard],
   bootstrap: [AppComponent]
